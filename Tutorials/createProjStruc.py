691badc7b87f5c3484328c3c3ccf9f02f1115aaf
MarcAstner <60645657+MarcAstner@users.noreply.github.com>
2021-06-06 22:42:33 +0200

    Update createProjStruc.py

--- Tutorials/createProjStruc.py
+++ Tutorials/createProjStruc.py
@@ -1,24 +1,26 @@
 from datetime import date, timedelta
 import sys
 
-sdate = date(2021, 1, 4)   # start date
-edate = date(2021, 2, 1)   # end date
+sdate = date(2021, 6, 7)   # start date
+edate = date(2021, 6, 30)   # end date
 
 delta = edate - sdate       # as timedelta
 
 students = [
-"Adjaye",
-"Francesco",
-"Jonas",
-"Juliette",
-"Marco",
+"Chris",
+"Gabriel",
+"Justus",
+"Linus",
+"Lucas",
+"Marc",
+"Milan",
 "Timon",
-"Sushant"
+"Xavier"
 ]
 
 
 def printProgressReport():
-    f = open("ProgressReport_CRAPy.md", "w")
+    f = open("PROGRESS:REPORT.md", "w")
 
     f.write("# Progress report of the CRAPy project\n\n")
 
@@ -31,13 +33,13 @@ def printProgressReport():
     f.close()
 
 def printProgressReportContent():
-    f = open("ProgressReport_Table_CRAPy.md", "w")
+    f = open("PROGRESS:REPORT:TABLE.md", "w")
 
     f.write("# Project Daily Progress Report\n\n## Table of Content\n\n")
 
 
     for i in range(delta.days + 1):
         day = sdate + timedelta(days=i)
-        f.write("[**Daily Report - {} : {}**](https://github.com/Jonastjep/CRAPy/blob/master/ProgressReport_CRAPy.md#progress-{}-{}-{}-{})\n\n".format(day.strftime("%d/%m/%Y"),day.strftime("%A %d %B, %Y"),day.strftime("%A").lower(),day.strftime("%d"),day.strftime("%B").lower(),day.strftime("%Y")))
+        f.write("[**Daily Report - {} : {}**](https://github.com/Jonastjep/CRAPy/blob/master/PROGRESS:REPORT.md#progress-{}-{}-{}-{})\n\n".format(day.strftime("%d/%m/%Y"),day.strftime("%A %d %B, %Y"),day.strftime("%A").lower(),day.strftime("%d"),day.strftime("%B").lower(),day.strftime("%Y")))
 
     f.close()
